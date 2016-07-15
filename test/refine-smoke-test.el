0dd204b51f182d5aefd8099666d767dac01014a4
Wilfred Hughes <me@wilfred.me.uk>
2016-07-15 22:58:08 +0100

    Add test for refine--variables

--- test/refine-smoke-test.el
+++ test/refine-smoke-test.el
@@ -4,3 +4,8 @@
 (ert-deftest refine-auto-mode-alist ()
   "Smoke test to ensure that we can show a complex list."
   (refine 'auto-mode-alist))
+
+(ert-deftest refine-variables-not-functions ()
+  (let ((vars (refine--variables)))
+    (should (-contains-p vars 'kill-ring))
+    (should (not (-contains-p vars 'message)))))
