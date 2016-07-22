d66deed97a801353a60f5290425880f6f5c49b47
Wilfred Hughes <me@wilfred.me.uk>
2016-07-22 13:43:45 +0100

    Name the test variable consistently in all test files.

--- test/refine-datastructure-test.el
+++ test/refine-datastructure-test.el
@@ -1,39 +1,38 @@
 (require 'ert)
 (require 'refine)
 
-;; It's much easier to have a global variable to test against, sadly.
-(defvar test-list nil)
+(defvar refine--test-var)
 
 (ert-deftest refine-insert ()
-  (setq test-list (list 1 2 3))
-  (let ((original test-list))
-    (refine--insert 'test-list 1 "foo")
+  (setq refine--test-var (list 1 2 3))
+  (let ((original refine--test-var))
+    (refine--insert 'refine--test-var 1 "foo")
     ;; We should have inserted the value.
-    (should (equal test-list (list 1 "foo" 2 3)))
+    (should (equal refine--test-var (list 1 "foo" 2 3)))
     ;; We should have mutated in place, so we still have the same cons
     ;; cell.
-    (should (eq test-list original))
+    (should (eq refine--test-var original))
     ;; The tails should also be the same cons cell.
-    (should (eq (last test-list) (last original)))))
+    (should (eq (last refine--test-var) (last original)))))
 
 (ert-deftest refine-list-insert-at-end ()
   "Test we handle the append case correctly."
-  (setq test-list (list 1 2 3))
-  (let ((original test-list))
-    (refine--insert 'test-list 3 "foo")
-    (should (equal test-list (list 1 2 3 "foo")))))
+  (setq refine--test-var (list 1 2 3))
+  (let ((original refine--test-var))
+    (refine--insert 'refine--test-var 3 "foo")
+    (should (equal refine--test-var (list 1 2 3 "foo")))))
 
 (ert-deftest refine-pop ()
-  (setq test-list (list 1 2 3))
-  (let ((original test-list))
-    (refine--pop 'test-list 1)
+  (setq refine--test-var (list 1 2 3))
+  (let ((original refine--test-var))
+    (refine--pop 'refine--test-var 1)
     ;; We should have popped the value
-    (should (equal test-list (list 1 3)))
+    (should (equal refine--test-var (list 1 3)))
     ;; We should have mutated in place, so we still have the same cons
     ;; cell.
-    (should (eq test-list original))
+    (should (eq refine--test-var original))
     ;; The tails should also be the same cons cell.
-    (should (eq (last test-list) (last original)))))
+    (should (eq (last refine--test-var) (last original)))))
 
 (ert-deftest refine-move-element ()
   (let ((my-list '(a b c d e)))
